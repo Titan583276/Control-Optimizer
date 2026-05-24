2bf50ebd98bbced0b321a445e556e2d84d0cef10
Thomas Stähli <staehtho@students.zhaw.ch>
2026-05-24 11:21:47 +0200

    todos

--- TODOs.py
+++ TODOs.py
@@ -1,2 +1,4 @@
 # TODO: Meinung von Thomas: da der Code veröffentlicht wird, sollten am Ende alle nicht relevanten Skripte entweder in
 #   in das Doku-Repo verschoben oder gelöscht werden (PlayGround)
+
+# TODO: Header in jedem script!!!
