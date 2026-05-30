9833e800a0a90d1e6b3fda57111657b4e672a87b
flo <buechfl2@students.zhaw.ch>
2026-05-30 21:15:35 +1200

    deleted todos

--- TODOs.py
+++
@@ -1,2 +0,0 @@
-# TODO: Meinung von Thomas: da der Code veröffentlicht wird, sollten am Ende alle nicht relevanten Skripte entweder in
-#   in das Doku-Repo verschoben oder gelöscht werden (PlayGround)
